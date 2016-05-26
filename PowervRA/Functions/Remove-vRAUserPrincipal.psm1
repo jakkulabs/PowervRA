6505185c36386200765b75826044421fbc6e7d52
Craig Gumbley <craiggumbley@gmail.com>
2016-05-26 13:22:38 +0100

    Updates for New-vRAUserPrincipal and Get-vRAUSerPrincipal. Initial commits for Remove and Set

---
+++ PowervRA/Functions/Remove-vRAUserPrincipal.psm1
@@ -0,0 +1,69 @@
+﻿function Remove-vRAUserPrincipal {
+<#
+    .SYNOPSIS
+    Remove a vRA local user principal
+    
+    .DESCRIPTION
+    Remove a vRA local user principal
+    
+    .PARAMETER PrincipalId
+    The principal id of the user
+
+    .INPUTS
+    System.String.
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Remove-vRAUserPrincipal -PrincipalId user@vsphere.local
+    
+    .EXAMPLE
+    Get-vRAUserPrincipal -Id user@vsphere.local | Remove-vRAUserPrincipal
+#>
+[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+
+    Param (
+
+    [parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [ValidateNotNullOrEmpty()]
+    [Alias("PrincipalId")]
+    [String[]]$Id
+    
+    )    
+
+    begin {
+    
+    }
+    
+    process {    
+            
+        foreach ($PrincipalId in $Id){
+                
+            try {
+                
+                if ($PSCmdlet.ShouldProcess($PrincipalId)){
+                    
+                    # --- Get the user principal object
+                    $User = Get-vRAUserPrincipal -Id $PrincipalId
+
+                    $URI = "/identity/api/tenants/$($User.TenantName)/principals/$($PrincipalId)"  
+                    
+                    Write-Verbose -Message "Preparing DELETE to $($URI)"                        
+
+                    # --- Run vRA REST Request                    
+                    Invoke-vRARestMethod -Method DELETE -URI $URI | Out-Null
+                    
+                }
+                
+            }
+            catch [Exception]{
+
+                throw
+            } 
+        }
+    }
+    end {
+        
+    }
+}
