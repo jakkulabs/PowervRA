5f578ea00cd70228163a12c87684e3c756091c63
Craig Gumbley <craiggumbley@gmail.com>
2016-10-01 20:12:21 +0100

    Removed Test task from Release

--- bin/psake.build.ps1
+++ bin/psake.build.ps1
@@ -9,8 +9,8 @@ Properties {
 }
 
 Task Default -depends Build
-Task Build -depends Analyze, UpdateFunctions, UpdateDocumentation
-Task Release -depends Test, Build, BumpVersion
+Task Build -depends Analyze, UpdateFunctions, UpdateDocumentation, 
+Task Release -depends Build, BumpVersion
 
 Task UpdateFunctions {
 
