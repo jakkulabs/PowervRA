4374cb542f5a1855c691602900c4359952c0cace
Craig Gumbley <craiggumbley@gmail.com>
2016-10-01 18:21:53 +0100

    Cleans up code

--- PowervRA/Functions/Public/iaas-proxy-provider/New-vRANetworkProfileIPRange.psm1
+++ PowervRA/Functions/Public/iaas-proxy-provider/New-vRANetworkProfileIPRange.psm1
@@ -35,104 +35,40 @@
 
     Param (
 
-    [parameter(Mandatory=$true)]
-    [ValidateNotNullOrEmpty()]
-    [String]$Name,
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Name,
 
-    [parameter(Mandatory=$false)]
-    [ValidateNotNullOrEmpty()]
-    [String]$Description,
+        [Parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Description,
 
-    [parameter(Mandatory=$true)]
-    [ValidateScript({$_ -match [IPAddress]$_ })]  
-    [String]$StatIPv4Address,
-    
-    [parameter(Mandatory=$true)]
-    [ValidateScript({$_ -match [IPAddress]$_ })]  
-    [String]$EndIPv4Address
-
-    )                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-
-    begin {
-    
-    }
-    
-    process {
-
-        try {
-
-            # --- Define ip address range
-            $IPAddressRange = [pscustomobject] @{
-
-                    name = "$($Name)"
-                    description =  "$($Description)"
-                    beginIPv4Address =  "$($StatIPv4Address)"
-                    endIPv4Address = "$($EndIPv4Address)"
-                    state = "UNALLOCATED"
-                    networkProfileId = "$($NetworkProfileId)"
-                    createdDate = $null
-                    lastModifiedDate = $null
-                    definedAddresses = @()
-                }
-    
-            <# --- Calculate and add the definedAddresses - It looks like this is actually done automatically so probably not needed
-
-            # --- Thanks to Dr. Tobias Weltner for this  >> http://powershell.com/cs/media/p/9437.aspx
-            Write-Verbose -Message "Calculating IP Addresses For Range $($StatIPv4Address) -> $($EndIPv4Address)"
-
-            $IP1 = ([System.Net.IPAddress]$StatIPv4Address).GetAddressBytes() 
-            
-            [Array]::Reverse($IP1) 
-            
-            $IP1 = ([System.Net.IPAddress]($IP1 -join '.')).Address 
-   
-            $IP2 = ([System.Net.IPAddress]$EndIPv4Address).GetAddressBytes() 
-  
-            [Array]::Reverse($IP2) 
-  
-            $IP2 = ([System.Net.IPAddress]($IP2 -join '.')).Address 
-   
-            for ($X=$IP1; $X -le $IP2; $X++) { 
-    
-                $IP = ([System.Net.IPAddress]$X).GetAddressBytes()
-                 
-                [Array]::Reverse($IP)
-                
-                $IPAddress = [pscustomobject] @{
-
-                    id = $null
-                    name = $null
-                    description =  $null
-                    networkProfileId = $null
-                    staticIPv4RangeId = $null
-                    virtualMachineId = $null
-                    IPv4Address = "$($IP -join '.')"
-                    IPSortValue = 0
-                    state = "UNALLOCATED"
-                    networkInterfaceCardOffset = $null
-                    hostName = $null
-                    createdDate = $null
-                    lastModifiedDate = $null
-
-                }                  
-                
-                $IPAddressRange.definedAddresses += $IPAddress               
-                      
-            }
-            
-            #>
-
-            # --- Return the new ip address range
-            $IPAddressRange
-
-        }
-        catch [Exception]{
-
-            throw
-        }
-
-    }
-    end {
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]  
+        [String]$StatIPv4Address,
         
-    }
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]  
+        [String]$EndIPv4Address
+
+    )
+
+        # --- Define ip address range
+        $IPAddressRange = [PSCustomObject] @{
+
+                name = $Name
+                description = $Description
+                beginIPv4Address = $StatIPv4Address
+                endIPv4Address = $EndIPv4Address
+                state = "UNALLOCATED"
+                networkProfileId = $NetworkProfileId
+                createdDate = $null
+                lastModifiedDate = $null
+                definedAddresses = @()
+
+            }
+
+        # --- Return the new ip address range
+        $IPAddressRange
+
 }
