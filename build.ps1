62d58f667bbc1088cb48e627301549be6f73d0bb
Craig Gumbley <craiggumbley@gmail.com#>
2016-11-10 22:32:33 +0000

    PlatyPS integration

--- build.ps1
+++ build.ps1
@@ -28,9 +28,7 @@ Param (
 $LocalDependencies = @(
 
     "$($PSScriptRoot)\bin\Update-MKDocsYML.psm1",
-    "$($PSScriptRoot)\bin\Update-ModuleDocumentation.psm1",
-    "$($PSScriptRoot)\bin\Update-ModuleManifestVersion.psm1",
-    "$($PSScriptRoot)\bin\Update-ModuleManifestFunctions.psm1"
+    "$($PSScriptRoot)\bin\Update-ModuleManifestVersion.psm1"
 
 )
 
