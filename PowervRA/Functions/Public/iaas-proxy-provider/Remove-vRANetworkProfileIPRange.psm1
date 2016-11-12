c7264646dda74ceebad23d825d131b4d0d786bd3
Craig Gumbley <craiggumbley@gmail.com#>
2016-11-12 22:52:57 +0000

    VerbosePreference

--- PowervRA/Functions/Public/iaas-proxy-provider/Remove-vRANetworkProfileIPRange.psm1
+++ PowervRA/Functions/Public/iaas-proxy-provider/Remove-vRANetworkProfileIPRange.psm1
@@ -47,7 +47,7 @@
             foreach ($NetworkProfileName in $Name) {
 
                 # --- Get the network profile to modify
-                $NetworkProfile = Invoke-vRARestMethod -Method GET -URI "/iaas-proxy-provider/api/network/profiles/$($NetworkProfileId)"
+                $NetworkProfile = Invoke-vRARestMethod -Method GET -URI "/iaas-proxy-provider/api/network/profiles/$($NetworkProfileId)" -Verbose:$VerbosePreference
 
                 #--- Test to see if the specified range exists
 
@@ -59,17 +59,14 @@
 
                 # --- Creae a new array
                 Write-Verbose -Message "Removing IP Range $($NetworkProfileName) From Network Profile $($NetworkProfile.name)"
+                
                 $NetworkProfile.definedRanges = @($NetworkProfile.definedRanges | Where-Object {$_.name -ne $NetworkProfileName})
 
                 if ($PSCmdlet.ShouldProcess($NetworkProfileId)){
 
                     $URI = "/iaas-proxy-provider/api/network/profiles/$($NetworkProfileId)"
             
-                    Write-Verbose -Message "Preparing PUT to $($URI)"
-
-                    Invoke-vRARestMethod -Method PUT -URI "$($URI)" -Body ($NetworkProfile | ConvertTo-Json -Depth 20 -Compress) | Out-Null
-
-                    Write-Verbose -Message "SUCCESS"
+                    Invoke-vRARestMethod -Method PUT -URI "$($URI)" -Body ($NetworkProfile | ConvertTo-Json -Depth 20 -Compress) -Verbose:$VerbosePreference | Out-Null
 
                 }
       
