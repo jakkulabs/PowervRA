ccf65a9009c6283c069e62d3f09a0d9ba62ce8b0
Craig Gumbley <craiggumbley@gmail.com#>
2016-11-12 17:53:00 +0000

    Update build.settings.ps1

--- build.settings.ps1
+++ build.settings.ps1
@@ -4,18 +4,37 @@
 
 Properties {
 
+# ----------------------- Module properties --------------------------------
+
+    # --- Author of this module
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+    $ModuleAuthor = 'Jakku Labs'
+
+    # --- Company or vendor of this module
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+    $ModuleCompanyName = 'Jakku Labs'
+
+    # --- Copyright statement for this module
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+    $ModuleCopyright = '(c) 2016 Jakku Labs. All rights reserved.'
+
+    # --- Setting to use when calling the BumpVersion task. This can either be Major, Minor or Patch.
+    # --- This setting should eventually be parameterised
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+    $BumpVersion = "Patch"
+
 # ----------------------- Basic properties --------------------------------
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
     $ModuleDirectory = "$($PSScriptRoot)\PowervRA"
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
-    $ModuleManifest = "$($ModuleDirectory)\PowervRA.psd1"    
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
     $ModuleName = Get-Item $ModuleDirectory/*.psd1 |
                       Where-Object { $null -ne (Test-ModuleManifest -Path $_ -ErrorAction SilentlyContinue) } |
                       Select-Object -First 1 | Foreach-Object BaseName
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
+    $ModuleManifestPath = "$($ModuleDirectory)\$($ModuleName).psd1"    
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
     $DocsDirectory = "$PSScriptRoot\docs"
@@ -24,27 +43,20 @@ Properties {
     $TestDirectory = "$PSScriptRoot\tests"
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
-    $BuildHelpersDirectory = "$($PSScriptRoot)\BuildHelpers"
-
-    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
     $DefaultLocale = "en-US"
 
-    # Enable/disable use of PSScriptAnalyzer to perform script analysis.
-    [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
-    $ScriptAnalysisEnabled = $true
-
-    # When PSScriptAnalyzer is enabled, control which severity level will generate a build failure.
-    # Valid values are Error, Warning, Information and None.  "None" will report errors but will not
-    # cause a build failure.  "Error" will fail the build only on diagnostic records that are of
-    # severity error.  "Warning" will fail the build on Warning and Error diagnostic records.
-    # "Any" will fail the build on any diagnostic record, regardless of severity.
+    # --- When PSScriptAnalyzer is enabled, control which severity level will generate a build failure.
+    # --- Valid values are Error, Warning, Information and None.  "None" will report errors but will not
+    # --- cause a build failure.  "Error" will fail the build only on diagnostic records that are of
+    # --- severity error.  "Warning" will fail the build on Warning and Error diagnostic records.
+    # --- "Any" will fail the build on any diagnostic record, regardless of severity.
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
     [ValidateSet('Error', 'Warning', 'Any', 'None')]
     $ScriptAnalysisFailBuildOnSeverityLevel = 'Error'
 
     # Path to the PSScriptAnalyzer settings file.
     [System.Diagnostics.CodeAnalysis.SuppressMessage('PSUseDeclaredVarsMoreThanAssigments', '')]
-    $ScriptAnalyzerSettingsPath = "$PSScriptRoot\PSScriptAnalyzerSettings.psd1"    
+    $ScriptAnalyzerSettingsPath = "$PSScriptRoot\PSScriptAnalyzerSettings.psd1"
 
 # ------------------ Test Properties ---------------------------
 
