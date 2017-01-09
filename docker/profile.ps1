f7924561e368e12f05e56433ffe792c8213ed505
Craig Gumbley <craiggumbley@gmail.com>
2017-01-09 21:54:55 +0000

    Initial commit for PowervRA CORE docker image

---
+++ docker/profile.ps1
@@ -0,0 +1,10 @@
+<#
+
+    Replacement PowerShell profile for PowervRA Core 
+    docker image.
+
+#>
+
+Import-Module -Name PowervRA
+Write-Host "Welcome to PowervRA Core" -ForegroundColor Green
+Write-Host "https://github.com/jakkulabs/PowervRA" -ForegroundColor Green
