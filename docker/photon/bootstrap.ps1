e6fc4664f0d3a0942a22725eb80d439c80070bf4
Craig Gumbley <craiggumbley@gmail.com>
2017-04-24 13:05:42 +0100

    Removes dependency on bootstrap.ps1

--- docker/photon/bootstrap.ps1
+++
@@ -1,5 +0,0 @@
-# Bootstrap the image with the Nuget package provider
-# and the PowervRA module from the PowerShell Gallery
-
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Module -Name PowervRA -Confirm:$false
