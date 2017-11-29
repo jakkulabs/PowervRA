d3620077985f948b519cd01522834e08484499fa
Jonathan Medd <jmedd@vrademo.local>
2017-11-29 17:59:28 +0000

    Help updates to clarify resource type changes from vRA 7.1 -> 7.2

--- PowervRA/Functions/Public/reservation-service/Get-vRAReservationComputeResourceMemory.ps1
+++ PowervRA/Functions/Public/reservation-service/Get-vRAReservationComputeResourceMemory.ps1
@@ -8,6 +8,8 @@
 
     .PARAMETER Type
     The reservation type
+    Valid types vRA 7.1 and earlier: Amazon, Hyper-V, KVM, OpenStack, SCVMM, vCloud Air, vCloud Director, vSphere,XenServer
+    Valid types vRA 7.2 and later: Amazon EC2, Azure, Hyper-V (SCVMM), Hyper-V (Standalone), KVM (RHEV), OpenStack, vCloud Air, vCloud Director, vSphere (vCenter), XenServer
 
     .PARAMETER ComputeResourceId
     The id of the compute resource
@@ -19,8 +21,18 @@
     System.Management.Automation.PSObject
 
     .EXAMPLE
-    Get-vRAReservationComputeResourceMemory -Type vSphere -ComputeResourceId 0c0a6d46-4c37-4b82-b427-c47d026bf71d
+    # Retrieve associated compute resources for the desired reservation type in vRA 7.1
+    Get-vRAReservationComputeResource -Type 'vSphere' -Name "Cluster01 (vCenter)" | Select-Object -ExpandProperty Id
 
+    # Retrieve associated compute resource memory for the desired reservation type in vRA 7.1
+    Get-vRAReservationComputeResourceMemory -Type 'vSphere' -ComputeResourceId 0c0a6d46-4c37-4b82-b427-c47d026bf71d
+
+    .EXAMPLE
+    # Retrieve associated compute resources for the desired reservation type in vRA 7.2 and later
+    Get-vRAReservationComputeResource -Type 'vSphere (vCenter)' -Name "Cluster01 (vCenter)" | Select-Object -ExpandProperty Id
+
+    # Retrieve associated compute resource memory for the desired reservation type in vRA 7.2 and later
+    Get-vRAReservationComputeResourceMemory -Type 'vSphere (vCenter)' -ComputeResourceId 0c0a6d46-4c37-4b82-b427-c47d026bf71d
 #>
 [CmdletBinding(DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
 
