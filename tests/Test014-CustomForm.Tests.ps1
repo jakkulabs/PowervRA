5801b25b35925015139369d48e465faae8d25b19
Kasey Linden <klinden@vmware.com>
2019-01-22 12:22:03 -0600

    created tests for set-vracustomform, add-vracustomform, and remove-vracustomform; build success

--- tests/Test014-CustomForm.Tests.ps1
+++ tests/Test014-CustomForm.Tests.ps1
@@ -8,27 +8,61 @@ $Connection = Connect-vRAServer -Server $JSON.Connection.vRAAppliance -Tenant $J
 # --- Tests
 Describe -Name 'Custom Forms Tests' -Fixture {
 
-  It -Name "Get single form by id $($JSON.Blueprint.BlueprintId)" -Test {
+  It -Name "Get vRA Custom Form by blueprint id $($JSON.Blueprint.BlueprintId)" -Test {
 
-      $CustomFormA = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId
-      $CustomFormA.BlueprintId | Should -Be $JSON.Blueprint.BlueprintId
-      $CustomFormA.JSON | Should -BeOfType System.String
-      $CustomFormA.JSON | Should -BeLikeExactly '{*}'
-
-  }
-
-  It -Name "Get form from pipped Blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
-
-      $CustomFormB = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId | Get-vRACustomForm
-      $CustomFormB.BlueprintId | Should -Be $JSON.Blueprint.BlueprintId
-      $CustomFormB.JSON | Should -BeOfType System.String
-      $CustomFormB.JSON | Should -BeLikeExactly '{*}'
+      $GetCustomFormA = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId
+      $GetCustomFormA.BlueprintId | Should -Be $JSON.Blueprint.BlueprintId
+      $GetCustomFormA.JSON | Should -BeOfType System.String
+      $GetCustomFormA.JSON | Should -BeLikeExactly '{*}'
 
   }
 
-  It - Name "Enable Custom Form by id $($JSON.Blueprint.BlueprintId)" -Test {
-      $SetCustomFormA = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId | Set-vRACustomForm -Action enable
+  It -Name "Get form from pipped blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
+
+      $GetCustomFormB = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId | Get-vRACustomForm
+      $GetCustomFormB.BlueprintId | Should -Be $JSON.Blueprint.BlueprintId
+      $GetCustomFormB.JSON | Should -BeOfType System.String
+      $GetCustomFormB.JSON | Should -BeLikeExactly '{*}'
+
+  }
+
+  It -Name "Enable Custom Form of blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
+
+      $SetCustomFormA = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId | Set-vRACustomForm -Action enable -Confirm:$false
       $SetCustomFormA | Should -Be "Custom form is enabled."
+
   }
+
+  It -Name "Disable Custom Form of blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
+
+      $SetCustomFormB = Get-vRACustomForm -Id $JSON.Blueprint.BlueprintId | Set-vRACustomForm -Action disable -Confirm:$false
+      $SetCustomFormB | Should -Be "Custom form is disabled."
+
+  }
+
+  It -Name "Remove Custom Form of blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
+
+      $RemoveCustomFormA = Remove-vRACustomForm -Id $JSON.Blueprint.BlueprintId -Confirm:$false
+
+      try {
+
+          $Form = Get-vRACustomForm -BlueprintId $JSON.Blueprint.BlueprintId -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+
+      }
+      catch {}
+
+      $Form | Should -be
+
+  }
+
+  It -Name "Add Custom Form to blueprint by id $($JSON.Blueprint.BlueprintId)" -Test {
+
+      $AddCustomFormA = Add-vRACustomForm -Id $JSON.Blueprint.BlueprintId -Body $GetCustomFormA.JSON
+      $AddCustomFormA | Should -BeOfType System.Management.Automation.PSCustomObject
+      $AddCustomFormA.BlueprintId | Should -Be $JSON.Blueprint.BlueprintId
+      $AddCustomFormA.CustomFormId | Should -BeOfType System.String
+
+  }
+
 
 }
