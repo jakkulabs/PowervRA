fd00034b80499807262f8f2b4333f5701a68a471
Kasey Linden <klinden@vmware.com>
2019-01-22 10:44:54 -0600

    fixed variable name issue

--- src/Functions/Public/composition-service/Add-vRACustomForm.ps1
+++ src/Functions/Public/composition-service/Add-vRACustomForm.ps1
@@ -72,10 +72,10 @@ function Add-vRACustomForm {
 
             foreach ($bp in $BlueprintId){
                 $URI = "/composition-service/api/blueprints/$($bp)/forms/requestform"
-
+                $JSON = $Body
                 # --- Run vRA REST Request
                 Write-Verbose -Message "Removing vRA Custom Form for blueprint $($bp)"
-                $Response = Invoke-vRARestMethod -Method POST -URI $URI -Body $Body
+                $Response = Invoke-vRARestMethod -Method POST -URI $URI -Body $JSON
                 return StandardOutput($bp)($Response)
 
             }
