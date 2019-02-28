f126b4079a55f71e9aead73699da578b1635a9f2
Steve Carnes <stevencarnes54@gmail.com>
2019-02-28 15:12:37 -0800

    Adding Property Group functions to the properties-service

---
+++ src/Functions/Public/properties-service/New-vRAPropertyGroup.ps1
@@ -0,0 +1,208 @@
+﻿function New-vRAPropertyGroup {
+    <#
+    .SYNOPSIS
+    Create a custom Property Group
+    
+    .DESCRIPTION
+    Create a custom Property Group
+
+    .PARAMETER Name
+    The unique name (ID) of the Property
+    
+    .PARAMETER Label
+    The text to display in forms for the Property
+
+    .PARAMETER Description
+    Description of the Property
+
+    .PARAMETER Tenant
+    The tenant in which to create the Property Group (Defaults to the connection tenant )
+
+    .PARAMETER Properties
+    The tenant in which to create the Property Group (Defaults to the connection tenant )
+   
+    .PARAMETER JSON
+    Property Group to send in JSON format
+
+    .INPUTS
+    System.String.
+
+    .OUTPUTS
+    System.Management.Automation.PSObject
+
+    .EXAMPLE
+    # Create a simple property group with no properties addded
+    New-vRAPropertyGroup -Name one 
+    
+    .EXAMPLE
+    # Create a property group with a description and label
+    New-vRAPropertyGroup -Name OneWithDescription -Label "On With Description" -Description "This is one with a label and description"
+
+    .EXAMPLE
+    # Create a property group with some properties added in simple form
+    New-vRAPropertyGroup -Name OneWithPropetiesSimple -Label "One With Properties" -Properties @{"com.org.bool"=$false; "com.org.string"="string1"}
+
+    .EXAMPLE
+    # Create a property group with some properties added in the extended form
+    New-vRAPropertyGroup -Name OneWithPropertiesExt -Label "One With Properties" -Properties @{"com.org.bool"=@{"mandatory"=$true; "defaultValue"=$false;}; "com.org.encryptedandshowonform"=@{"encrypted"=$true; "visibility"=$true; "defaultValue": "Un-encrypted string";};}
+
+#> 
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = "Low")][OutputType('System.Management.Automation.PSObject')]
+
+    Param (
+        [parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Name,
+        
+        [parameter(Mandatory = $false)]    
+        [ValidateNotNullOrEmpty()]
+        [String]$Label = $Name,
+
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Description,
+
+        [parameter(Mandatory = $false)]    
+        [ValidateNotNullOrEmpty()]
+        [String]$Tenant = $Global:vRAConnection.Tenant,
+        
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "Properties")]
+        [ValidateNotNullOrEmpty()]
+        [hashtable]$Properties,
+
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ParameterSetName = "JSON")]
+        [ValidateNotNullOrEmpty()]
+        [String]$JSON
+
+    )
+
+    begin {
+
+        # --- Test for vRA API version
+        xRequires -Version 7.0
+    }
+    
+    process {
+
+        try {
+            # --- Set Body for REST request depending on ParameterSet
+            if ($PSBoundParameters.ContainsKey("JSON")) {
+                $Body = $JSON
+            }
+            else {
+                $propertiesRaw = ""
+                
+                # process properties sent in
+                foreach ($propKey in $Properties.Keys) {
+                    $prop = $Properties[$propKey]
+                    switch ($prop.GetType()) {
+                        "Hashtable" {
+                            $facets = ""
+                            foreach ($fKey in $prop.Keys) {
+                                $f = $prop[$fKey]
+                                switch ($fKey) {
+                                    "visibility" {
+                                        $facets += @"
+                                        "visibility": {
+                                            "type": "constant",
+                                            "value": {
+                                                "type": "boolean",
+                                                "value": $($f.toString().toLower())
+                                            }
+                                },
+"@
+                                    }
+                                    "encrypted" {
+                                        $facets += @"
+                                        "encrypted": {
+                                            "type": "constant",
+                                            "value": {
+                                                "type": "boolean",
+                                                "value": $($f.toString().toLower())
+                                            }
+                                },
+"@
+                                    }
+                                    "mandatory" {
+                                        $facets += @"
+                                        "mandatory": {
+                                            "type": "constant",
+                                            "value": {
+                                                "type": "boolean",
+                                                "value": $($f.toString().toLower())
+                                            }
+                                },
+"@
+                                    }
+                                    "defaultValue" {
+                                        $facets += @"
+                                        "defaultValue": {
+                                            "type": "constant",
+                                            "value": {
+                                                "type": "string",
+                                                "value": "$($f)"
+                                            }
+                                },
+"@
+                                    }
+                                }
+                            }
+                            $propertiesRaw += @"
+                            "$($propKey)": {
+                                "facets": { $($facets.Trim(',')) }
+                            },
+"@
+                            break
+                        }
+                        default {
+                            $propertiesRaw += @"
+                            "$($propKey)": {
+                                "facets": {
+                                    "defaultValue": {
+                                        "type": "constant",
+                                        "value": {
+                                            "type": "string",
+                                            "value": "$($prop)"
+                                        }
+                                    }
+                                }
+                            },
+"@
+                            break
+                        }
+                    }
+                }
+                # logic to build input
+                $Body = @"
+                {
+                    "id" : "$($Name)",
+                    "label" : "$($Label)",
+                    "description" : "$($Description)",
+                    "tenantId" : "$($Tenant)",
+                    "version": 0,
+                    "properties": { $($propertiesRaw.Trim(',')) }
+                }
+"@
+            }
+
+            $URI = "/properties-service/api/propertygroups"  
+
+            Write-Verbose -Message "Preparing POST to $($URI)"   
+
+            Write-Verbose -Message "Posting Body: $($Body)"  
+
+            # --- Run vRA REST Request  
+            if ($PSCmdlet.ShouldProcess($Id)) {
+                Invoke-vRARestMethod -Method POST -URI $URI -Body $Body | Out-Null
+                Get-vRAPropertyGroup -Id $Name
+            }
+        }
+        catch [Exception] {
+
+            throw            
+        }        
+    }
+    end {
+        
+    }    
+}
---
+++ src/Functions/Public/properties-service/Remove-vRAPropertyGroup.ps1
@@ -0,0 +1,85 @@
+﻿function Remove-vRAPropertyGroup {
+<#
+    .SYNOPSIS
+    Removes a Property Group from the specified tenant
+    
+    .DESCRIPTION
+    Uses the REST API to delete a property Group based on the Id supplied. If the Tenant is supplied it will delete the property for that tenant only.
+
+    .PARAMETER Id
+    The id of the property Group to delete
+
+    .PARAMETER Tenant
+    The tenant of the property Group to delete
+
+    .INPUTS
+    System.String
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    # Remove the property group "Hostname"
+    Remove-vRAPropertyGroup -Id Hostname
+
+    .EXAMPLE
+    # Remove the property group "Hostname" using the pipeline
+    Get-vRAPropertyGroup -Id Hostname | Remove-vRAPropertyGroup -Confirm:$false
+    
+    .EXAMPLE
+    # Remove the property group "Hostname" from the tenant "Development"
+    Remove-vRAPropertyGroup -Id "Hostname" -Tenant Development
+
+#>
+[CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+
+    Param (
+
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Id,
+
+        [parameter(Mandatory=$false)]    
+        [ValidateNotNullOrEmpty()]
+        [String]$Tenant
+
+    )
+
+    Begin {
+        # --- Test for vRA API version
+        xRequires -Version 7.0
+    }
+
+    Process {
+
+        try {
+            # Get-vRAPropertyGroup will throw a 404 error if it doesn't exist
+            if(Get-vRAPropertyGroup -Id $Id) {
+                
+                if ($PSCmdlet.ShouldProcess($Id)){
+
+                    $URI = "/properties-service/api/propertygroups/$($Id)"
+
+                    if($Tenant) { 
+                        $URI += "?tenantId=$($Tenant)"
+                    }
+
+                    $EscapedURI = [uri]::EscapeUriString($URI)
+
+                    Invoke-vRARestMethod -Method DELETE -URI $EscapedURI -Verbose:$VerbosePreference
+                }
+
+            }
+
+        }
+        catch [Exception]{
+
+            throw
+
+        }
+    }
+
+    End {
+
+    }
+}
