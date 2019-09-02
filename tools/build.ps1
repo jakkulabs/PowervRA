a99ef44445cd8f7a1d7af91947b75c7aae03a082
Craig Gumbley <craiggumbley@gmail.com>
2019-09-02 20:25:27 +0100

    Add confirm:$false to module install

--- tools/build.ps1
+++ tools/build.ps1
@@ -34,7 +34,7 @@ Param (
 # --- Install dependencies
 $RequiredModules = @("Psake", "PSScriptAnalyzer", "BuildHelpers")
 foreach ($Module in $RequiredModules) {
-    Install-Module -Name $Module -Scope CurrentUser
+    Install-Module -Name $Module -Scope CurrentUser -Confirm:$false
 }
 
 # --- Set Build Environment
