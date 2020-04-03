1f48251ff835c1847b3280ec2bce5923c5685bae
Craig Gumbley <craiggumbley@gmail.com>
2020-04-03 13:30:44 +0100

    Updates to build and release process (#214)
    * Move to stages
    
    * Update GitVersion
    
    * Add pipeline artifact
    
    * More artifact work
    
    * Prepare for tag based releases
    
    * Fixes job condition
    
    * Removes dependsOn
    
    * Use System.DefaultWorkingDirectory on ci stage
    
    * Modifies script path
    
    * Test tagging condition
    
    * tigger on tag
    
    * Update to use PS Test Gallery
    
    * Use test gallery
    
    * Depend on stage
    
    * Move condition to stage
    
    * Download all artifacts in a single task
    
    * Split out test and prod publish
    
    * Missing '
    
    * Remove )

--- tools/Publish-PSModule.ps1
+++ tools/Publish-PSModule.ps1
@@ -3,9 +3,9 @@
     Publish PowerShell Modules to the PowerShell Gallery from VSTS
 
     .DESCRIPTION
-    Publish PowerShell Modules to the PowerShell Gallery from VSTS. This script exists because there is currently a bug 
+    Publish PowerShell Modules to the PowerShell Gallery from VSTS. This script exists because there is currently a bug
     with the following task which causes the task to fail when executed on a VS2017 hosted agent.
-    
+
     https://github.com/kenakamu/vsts-tasks/tree/master/Tasks/PSGalleryPublisher
 
     All credit must go to kenakamu for the original script
@@ -19,42 +19,50 @@ param(
     [Parameter(Mandatory=$true)]
     [String]$Path,
     [Parameter(Mandatory=$false)]
-    [Switch]$CheckForExistingVersion
+    [Switch]$CheckForExistingVersion,
+    [Parameter(Mandatory=$false)]
+    [Switch]$PreRelease
 )
 
 try {
     $NugetPath = "c:\nuget"
+    $Repository = "PSGallery"
 
     # --- C:\nuget exists on VS2017 build agents. To avoid task failure, check whether the directory exists and only create if it doesn't/
     if (!(Test-Path -Path $NugetPath)) {
         Write-Verbose "$NugetPath does not exist on this system. Creating directory."
         New-Item -Path $NugetPath -ItemType Directory
     }
-    
+
     Write-Verbose "Download Nuget.exe to C:\nuget"
     Invoke-WebRequest -Uri "http://go.microsoft.com/fwlink/?LinkID=690216&clcid=0x409" -OutFile $NugetPath\Nuget.exe
-    
+
     Write-Verbose "Add C:\nuget as %PATH%"
     $PathEnv = [System.Environment]::GetEnvironmentVariable("path")
     $PathEnv = $PathEnv + ";" + $NugetPath
     [System.Environment]::SetEnvironmentVariable("path", $PathEnv)
-    
+
+    Write-Verbose "Create NuGet package provider"
+    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
+
+    if ($PreRelease.IsPresent){
+        Register-PackageSource -Name PoshTestGallery -Location https://www.poshtestgallery.com/api/v2/ -ProviderName PowerShellGet
+        $Repository = "PoshTestGallery"
+    }
+
     # --- If a module with the same version exists in the PowerShell Gallery, gracefully exit
     if ($CheckForExistingVersion.IsPresent) {
         $ModuleManifestLocation = Get-ChildItem -Path "$Path\*.psd1"
         $ModuleManifest = Import-PowerShellDataFile -Path $ModuleManifestLocation
-        $PSGalleryModule = Find-Module -Name $ModuleManifestLocation.BaseName -RequiredVersion $ModuleManifest.ModuleVersion -ErrorAction SilentlyContinue
+        $PSGalleryModule = Find-Module -Name $ModuleManifestLocation.BaseName -RequiredVersion $ModuleManifest.ModuleVersion -Repository $Repository -ErrorAction SilentlyContinue
         if ($PSGalleryModule) {
             Write-Host "$($ModuleManifestLocation.BaseName) version $($ModuleManifest.ModuleVersion) already exists in the PowerShell gallery. Skipping task."
             return
         }
     }
-    
-    Write-Verbose "Create NuGet package provider"
-    Install-PackageProvider -Name NuGet -Scope CurrentUser -Force
-    
+
     Write-Verbose "Publishing module"
-    Publish-Module -Path $Path -NuGetApiKey $ApiKey
+    Publish-Module -Path $Path -NuGetApiKey $ApiKey -Repository $Repository
 }
 catch {
     throw "$_"
