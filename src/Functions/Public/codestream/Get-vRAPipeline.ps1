fce9b60eec7bdf5612fe6751bb6edc4889767a58
sammcgeown <sam@mcgeown.co.uk>
2020-06-09 17:13:31 +0100

    Add variables

---
+++ src/Functions/Public/codestream/Get-vRAPipeline.ps1
@@ -0,0 +1,122 @@
+﻿function Get-vRAPipeline {
+<#
+    .SYNOPSIS
+    Retrieve vRA Code Stream Pipeline depending on input
+
+    .DESCRIPTION
+    Retrieve a list of vRA Code Stream Pipelines or a single Pipeline depending on input
+
+    .PARAMETER Id
+    The ID of the vRA Code Stream Pipeline
+
+    .PARAMETER Pipeline
+    The name of the Pipeline of the vRA Code Stream Pipeline
+
+    .PARAMETER Project
+    The name of the Project of the vRA Code Stream Pipeline
+
+    .OUTPUTS
+    System.Management.Automation.PSObject.
+
+    .EXAMPLE
+    Get-vRAPipeline
+
+    .EXAMPLE
+    Get-vRAPipeline -Id '96afe0f9-a2ac-4468-8671-a2ca6fdbca37'
+
+    .EXAMPLE
+    Get-vRAPipeline -Pipeline 'My Pipeline'
+
+    .EXAMPLE
+    Get-vRAPipeline -Project 'My Project'
+
+#>
+[CmdletBinding(DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
+
+    Param (
+
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName="ById")]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$Id,
+
+        [Parameter(Mandatory=$true,ParameterSetName="ByName")]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$Pipeline,
+
+        [Parameter(Mandatory=$true,ParameterSetName="ByProject")]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$Project
+
+    )
+    Begin {
+
+        $APIUrl = "/pipeline/api/pipelines"
+
+        function CalculateOutput {
+            foreach ($Record in $Response.documents.PsObject.Properties) {
+                [PSCustomObject]@{
+                    Name = $Record.value.name
+                    Project = $Record.value.project
+                    Id = $Record.value.id
+                    LastUpdated = $Record.value.updatedAt
+                    UpdatedBy = $Record.value._updatedBy
+                    State = $Record.value.state
+                }
+            }
+        }
+    }
+    Process {
+
+        try {
+
+            switch ($PsCmdlet.ParameterSetName) {
+
+                # --- Get Pipeline by its id
+                'ById' {
+                    foreach ($pipelineId in $Id) {
+                        $Response = Invoke-vRARestMethod -URI "$APIUrl`?`$filter=id eq '$pipelineId'" -Method GET
+                        CalculateOutput
+                    }
+
+                    break
+                }
+
+                # --- Get Pipeline by its pipeline name
+                'ByName' {
+                    foreach ($pipelineName in $Pipeline) {
+                        $Response = Invoke-vRARestMethod -URI "$APIUrl`?`$filter=name eq '$pipelineName'" -Method GET
+                        CalculateOutput
+                    }
+
+                    break
+                }
+
+                # --- Get Pipeline by its project name
+                'ByProject' {
+                    foreach ($projectName in $Project) {
+                        $Response = Invoke-vRARestMethod -URI "$APIUrl`?`$filter=project eq '$projectName'" -Method GET
+                        CalculateOutput
+                    }
+
+                    break
+                }
+                
+                # --- No parameters passed so return all Pipelines
+                'Standard' {
+                    $Response = Invoke-vRARestMethod -URI $APIUrl -Method GET
+                    CalculateOutput
+                }
+
+            }
+
+
+        }
+        catch [Exception]{
+
+            throw
+        }
+    }
+    End {
+
+    }
+}
---
+++ src/Functions/Public/codestream/Update-vRAVariable.ps1
@@ -0,0 +1,135 @@
+function Update-vRAVariable {
+    <#
+        .SYNOPSIS
+        Update a vRealize Automation Code Stream Variable
+
+        .DESCRIPTION
+        Update a vRealize Automation Code Stream Variable
+
+        .PARAMETER Id
+        vRealize Automation Code Stream Variable Id
+
+        .PARAMETER Description
+        A description of the vRealize Automation Code Stream Variable
+
+        .PARAMETER Value
+        vRealize Automation Code Stream Variable Value
+
+        .PARAMETER Type
+        vRealize Automation Code Stream Variable Type
+
+        .PARAMETER JSON
+        A JSON string with the body payload
+
+        .INPUTS
+        System.String
+
+        .OUTPUTS
+        System.Management.Automation.PSObject
+
+        .EXAMPLE
+        Update-vRAVariable -Name "My Variable" -Type REGULAR -Id 05352fc5-24c2-4ead-b6c0-5373e60a1b3d -Description "Updated from PowervRA!" -Value "New value"
+
+        .EXAMPLE
+        Get-vRAVariable -Variable "My Variable" | Update-vRAVariable -Value "New value" -Description "New Description"
+
+        .EXAMPLE
+        $JSON = @"
+            {
+                "name": "My Variable",
+                "description": "My variable description"
+                "type": "REGULAR",
+                "value": "testing1"
+            }
+    "@
+
+        $JSON | Update-vRAVariable -Id 05352fc5-24c2-4ead-b6c0-5373e60a1b3d
+
+
+    #>
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact="Low",DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
+
+        Param (
+
+            [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ParameterSetName="Standard")]
+            [ValidateNotNullOrEmpty()]
+            [String]$Name,
+
+            [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ParameterSetName="Standard")]
+            [parameter(ParameterSetName = "JSON")]
+            [ValidateNotNullOrEmpty()]
+            [String]$Id,
+
+            [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName,ParameterSetName="Standard")]
+            [ValidateNotNullOrEmpty()]
+            [String]$Description,
+
+            [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ParameterSetName="Standard")]
+            [ValidateSet("REGULAR","SECRET","RESTRICTED")] 
+            [ValidateNotNullOrEmpty()]
+            [String]$Type,
+
+            [Parameter(Mandatory=$true,ParameterSetName="Standard")]
+            [ValidateNotNullOrEmpty()]
+            [String]$Value,
+
+            [Parameter(Mandatory=$true,ValueFromPipeline=$true,ParameterSetName="JSON")]
+            [ValidateNotNullOrEmpty()]
+            [String]$JSON
+
+        )
+
+        begin { }
+
+        process {
+
+                if ($PSBoundParameters.ContainsKey("JSON")) {
+                    $Body = $JSON
+                } else {
+                    if($Description) {
+                        $Body = @"
+                        {
+                            "name": "$($Name)",
+                            "description": "$($Description)",
+                            "type": "$($Type)",
+                            "value": "$($Value)"
+                        }
+"@
+                    } else {
+                    $Body = @"
+                        {
+                            "name": "$($Name)",
+                            "type": "$($Type)",
+                            "value": "$($Value)"
+                        }
+"@
+                    }
+                }
+
+            # --- Update the Variable
+            try {
+                if ($PSCmdlet.ShouldProcess($Id)){
+
+                    $URI = "/pipeline/api/variables/$($Id)"
+                    $Variable = Invoke-vRARestMethod -Method PUT -URI $URI -Body $Body -Verbose:$VerbosePreference
+
+                    [PSCustomObject] @{
+
+                        Name = $Variable.name
+                        Description = $Variable.description
+                        Id = $Variable.id
+                        Type = $Variable.type
+                        Value = $Variable.value
+                        Project = $Variable.project
+                    }
+                }
+            }
+            catch [Exception] {
+
+                throw
+            }
+        }
+        end {
+
+        }
+    }
