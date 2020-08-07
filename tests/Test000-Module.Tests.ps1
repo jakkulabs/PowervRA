3933df10ac602343373d8bbef9db1a9d6349b6dd
Craig Gumbley <craiggumbley@gmail.com>
2020-08-07 16:02:20 +0100

    Test beforeall in describe

--- tests/Test000-Module.Tests.ps1
+++ tests/Test000-Module.Tests.ps1
@@ -1,29 +1,39 @@
-﻿BeforeAll {
-    $ModulePath = (Resolve-Path -Path $ENV:BHProjectPath\Release\PowervRA\PowervRA.psd1).Path
-    Import-Module $ModulePath -Force
-}
+﻿# --- Ensure that each function has valid help and parameter descriptions
+Describe "Function Help -> " {
 
-# --- Ensure that each function has valid help
-Describe "Function Help -> " -Tags Help {
+    BeforeAll {
+        $ModulePath = (Resolve-Path -Path $ENV:BHProjectPath\Release\PowervRA\PowervRA.psd1).Path
+        Import-Module $ModulePath -Force
 
-    $Functions = @(Get-Command -Module PowervRA -CommandType Function | ForEach-Object { @{Name = $_.Name } })
+        $Functions = @(Get-Command -Module PowervRA -CommandType Function | ForEach-Object { @{Name = $_.Name } })
+
+    }
+
+    AfterAll {
+        Remove-Module -Name PowervRA -Force
+    }
+
 
     It "<Name> has the required help entries" -TestCases $Functions {
         Param(
             [string]$Name
         )
-        (Get-Help -Name $Name).Synopsis | Should -Not -BeNullOrEmpty
-        (Get-Help -Name $Name).Description | Should -Not -BeNullOrEmpty
-        (Get-Help -Name $Name).Examples | Should -Not -BeNullOrEmpt
+
+        $Help = Get-Help -Name $Name
+
+        $Help.Synopsis | Should -Not -BeNullOrEmpty
+        $Help.Description | Should -Not -BeNullOrEmpty
+        $Help.Examples | Should -Not -BeNullOrEmpt
     }
 
     It "<Name> has documentation for all parameters" -TestCases $Functions {
         Param(
             [string]$Name
         )
-        $Help = Get-Help -Name $Name
-        foreach ($Parameter in $Help.parameters.parameter) {
 
+        $Help = Get-Help -Name $Name
+
+        foreach ($Parameter in $Help.parameters.parameter) {
             if ($Parameter -notmatch 'whatif|confirm') {
                 $Parameter.Description.text | Should -Not -BeNullOrEmpty
             }
