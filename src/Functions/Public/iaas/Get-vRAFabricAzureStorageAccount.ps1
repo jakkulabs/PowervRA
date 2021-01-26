0554a4829e3d4b2ac51ab991040c338c15cc6d7f
Marcus Puckett <mpuckett259@gmail.com>
2021-01-26 11:58:16 -0800

    Complete Get-vRAFabricAzureStorageAccount function
    Complete the skeleton function that was created for the Get-vRAFabricAzureStorageAccount function. This function fetches any information related to a Azure Storage Account and returns it as a Powershell Custom Object
    
    The object properties returned are:
     * Type
     * ExternalRegionId
     * Name
     * Id
     * OrganizationId
     * CloudAccountIds
     * Links

--- src/Functions/Public/iaas/Get-vRAFabricAzureStorageAccount.ps1
+++ src/Functions/Public/iaas/Get-vRAFabricAzureStorageAccount.ps1
@@ -14,23 +14,40 @@ function Get-vRAFabricAzureStorageAccount {
     
         .EXAMPLE
         Get-vRAFabricAzureStorageAccounts
+
+        .EXAMPLE
+        Get-vRAFabricAzureStorageAccounts -Id 1c3a50ca-1c38-42a3-a572-209d5dfdecf7
+
+        .EXAMPLE
+        Get-vRAFabricAzureStorageAccounts -Name "test-Azure-Storage-Account"
     
     #>
     [CmdletBinding(DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
     
         Param (
+            [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName="ById")]
+            [ValidateNotNullOrEmpty()]
+            [String[]]$Id,
+
+            [Parameter(Mandatory=$true,ParameterSetName="ByName")]
+            [ValidateNotNullOrEmpty()]
+            [String[]]$Name
         )
     
         begin {
             $APIUrl = '/iaas/api/fabric-azure-storage-accounts'
     
             function CalculateOutput([PSCustomObject]$FabricAzureStorageAccounts) {
-
-                # Don't have any Azure accounts so not sure what this is supposed to look like, will test later
-                # TODO find examples of what these objects look like
     
                 [PSCustomObject] @{
-                    FabricAzureStorageAccount = $FabricAzureStorageAccounts
+
+                    Type = $FabricAzureStorageAccounts.type
+                    ExternalRegionId = $FabricAzureStorageAccounts.externalRegionId
+                    Name = $FabricAzureStorageAccounts.name
+                    Id = $FabricAzureStorageAccounts.id
+                    OrganizationId = $FabricAzureStorageAccounts.organizationId
+                    CloudAccountIds = $FabricAzureStorageAccounts.cloudAccountIds
+                    Links = $FabricAzureStorageAccounts._links
                 }
             }
         }
@@ -41,17 +58,55 @@ function Get-vRAFabricAzureStorageAccount {
     
                 switch ($PsCmdlet.ParameterSetName) {
     
-                    # --- Return all Fabric AWS Volume Types
-                    'Standard' {
-    
+                    # --- Get Cloud Account by id
+                    'ById' {
+
+                        foreach ($FabricAzureStorageAccountId in $Id) {
+
+                            $URI = "$APIUrl/$($FabricAzureStorageAccountId)"
+
+                            $FabricAzureStorageAccount = Invoke-vRARestMethod -Method GET -URI $URI -Verbose:$VerbosePreference
+
+                            CalculateOutput $FabricAzureStorageAccount
+                        }
+
+                        break
+                    }
+                    # --- Get Cloud Account by name
+                    'ByName' {
+
                         $URI = $APIUrl
+
                         $Response = Invoke-vRARestMethod -Method GET -URI $URI -Verbose:$VerbosePreference
 
-                        foreach ($FabricAzureStorageAccounts in $Response.content) {
+                        foreach ($FabricAzureStorageAccountName in $Name) {
+
+                            $FabricAzureStorageAccounts = $Response.content | Where-Object {$_.name -eq $FabricAzureStorageAccountName}
+
+                            if (!$FabricAzureStorageAccounts) {
+
+                                throw "Could not find Cloud Account with name: $($FabricAzureStorageAccounts)"
+
+                            }
+
                             CalculateOutput $FabricAzureStorageAccounts
                         }
+
+                        break
                     }
-                }
+                        # --- Return all Fabric AWS Volume Types
+                        'Standard' {
+        
+                            $URI = $APIUrl
+
+                            $Response = Invoke-vRARestMethod -Method GET -URI $URI -Verbose:$VerbosePreference
+
+                            foreach ($FabricAzureStorageAccounts in $Response.content) {
+
+                                CalculateOutput $FabricAzureStorageAccounts
+                            }
+                        }
+                    }
             }
             catch [Exception]{
     
