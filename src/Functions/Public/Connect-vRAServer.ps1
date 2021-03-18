d49732e15a3bfdf509d21578ad2477b803b99670
Stevio54 <stevencarnes54@gmail.com>
2021-03-18 12:43:14 -0700

    added new functionality to connect function

--- src/Functions/Public/Connect-vRAServer.ps1
+++ src/Functions/Public/Connect-vRAServer.ps1
@@ -88,10 +88,6 @@
             [String]$APIToken,
 
             [parameter(Mandatory=$false)]
-            [ValidateSet('sAMAccountName', 'SAM', 'userPrincipalName', 'UPN')]
-            [String]$UserAttribute = 'SAM',
-
-            [parameter(Mandatory=$false)]
             [Switch]$IgnoreCertRequirements,
 
             [parameter(Mandatory=$false)]
@@ -176,9 +172,9 @@
                     }
 
                     # --- Logging in with a domain
-                    if (@("SAM", "SAMAccountName").Contains($UserAttribute) -and $Username -match '@') {
+                    $URI = "https://$($Server)/csp/gateway/am/idp/auth/login?access_token"
+                    if ($Username -match '@') {
                         # Log in using the advanced authentication API
-                        $URI = "https://$($Server)/csp/gateway/am/idp/auth/login?access_token"
                         $User = $Username.split('@')[0]
                         $Domain = $Username.split('@')[1]
                         $JSON = @{
@@ -186,14 +182,21 @@
                             password = $JSONPassword
                             domain = $Domain
                         } | ConvertTo-Json
+
+                        # The alternate is to include the @[DOMAIN] in the username to cover all possible configurations
+                        $AlternateJson = @{
+                            username = $Username
+                            password = $JSONPassword
+                            domain = $Domain
+                        } | ConvertTo-Json
                     } else {
                         # -- Login with the basic authentication API
-                        $URI = "https://$($Server)/csp/gateway/am/api/login?access_token"
-
+                        # -- We assume local account which can use the advanced authentication API with domain set to 'System Domain'
                         # --- Create Invoke-RestMethod Parameters
                         $JSON = @{
                             username = $Username
                             password = $JSONPassword
+                            domain = "System Domain"
                         } | ConvertTo-Json
                     }
                 }
@@ -221,16 +224,49 @@
 
                 }
 
-                $Response = Invoke-RestMethod @Params
+                # Here we try with the first set of params, if it fails, it may be due to another configuration
+                # so we try again with the alternative option if available
+                try {
+                    $Response = Invoke-RestMethod @Params
+                } catch [Exception]{
+                    if ($_.Exception.Response.StatusCode -eq "BadRequest" -and $null -ne $AlternateJson) {
+                        $Params = @{
+
+                            Method = "POST"
+                            URI = $URI
+                            Headers = @{
+                                "Accept"="application/json";
+                                "Content-Type" = "application/json";
+                            }
+                            Body = $AlternateJson
+                        }
+                        $Response = Invoke-RestMethod @Params
+                    } else {
+                        throw $_
+                    }
+                }
 
                 if ('refresh_token' -in $Response.PSobject.Properties.Name) {
-                    $Token = $Response.access_token
                     $RefreshToken = $Response.refresh_token
                 }
 
-                if ('token' -in $Response.PSobject.Properties.Name) {
-                    $Token = $Response.token
-                    $RefreshToken = $APIToken
+                # now we need to login via the iaas api as well with the refresh token
+                $IaasParams = @{
+                    Method = "POST"
+                    URI = "https://$($Server)/iaas/api/login"
+                    Headers = @{
+                        "Accept"="application/json";
+                        "Content-Type" = "application/json";
+                    }
+                    Body = @{
+                        refreshToken = $RefreshToken
+                    } | ConvertTo-Json
+                }
+
+                $IaasResponse = Invoke-RestMethod @IaasParams
+
+                if ('token' -in $IaasResponse.PSobject.Properties.Name) {
+                    $Token = $IaasResponse.token
                 }
 
                 # --- Create Output Object
