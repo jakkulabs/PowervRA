9be010b3b99fefea16bf7400d4ed0803e0185fcf
Stevio54 <stevencarnes54@gmail.com>
2021-03-21 19:31:14 -0700

    after testing, some small bug fixes

--- test.ps1
+++
@@ -1,8 +0,0 @@
-$ProfileArgumenst = @{
- Name = 'Test Profile 2'
- Description = 'Test Profile 2'
- RegionId = '26e6d4a0-50c6-481f-b6ca-f4a6117874f9'
- Tags = 'placement:dude'
-}
-
-New-vRANetworkProfile @ProfileArgumenst
